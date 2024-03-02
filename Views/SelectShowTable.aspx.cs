1239c4157b9fca70dd4c73c859c897b5ac3d9182
tamar-avivi <tamar.avivi.24.2@gmail.com>
2024-03-02 18:33:47 +0200

    select show table

---
+++ Views/SelectShowTable.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Taba.Views
+{
+    public partial class SelectShowTable : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
